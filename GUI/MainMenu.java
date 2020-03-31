8bef48d247f253a526ff375674629160bbbe2415
cmots <1061129075@qq.com>
2020-03-31 17:08:00 +0800

    change MainMenu

--- GUI/MainMenu.java
+++ GUI/MainMenu.java
@@ -14,7 +14,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.SpringLayout;
-import net.miginfocom.swing.MigLayout;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
